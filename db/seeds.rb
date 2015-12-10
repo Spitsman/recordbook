5b2b1d2c9b16259348d4957c0b7012f93c2b6798
Spitsman <sanspits@yandex.ru>
2015-12-10 20:15:54 +0300

    update seed and readme

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,5 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 admin = User.create(first_name: "admin", last_name: "admin", role: "admin", email: "admin@mail.ru", password: 'qwerty')
+grehan = User.create(first_name: "grehan", last_name: "solovey", role: "student", email: "grehan@mail.ru", password: 'qwerty')
+sanya = User.create(first_name: "sanya", last_name: "spitsman", role: "student", email: "sanya@mail.ru", password: 'qwerty')
