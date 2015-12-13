fd39371248e436db49053256a954752c96afe075
Spitsman <sanspits@yandex.ru>
2015-12-13 13:39:21 +0300

    небольшой рефакторинг

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,21 +1,12 @@
 class UsersController < ApplicationController
 
-	helper_method :resource_user, :users_collection
+	helper_method :resource_user, :users_collection, :semesters_collection
 
 	before_action :require_user, only: [:show, :all]
 	before_action :require_admin, only: [:all, :create, :new, :destroy, :update]
 
 	def show
-		if current_user.admin?
-			if (params[:id] == nil)
-				@user = current_user
-				redirect_to admin_panel_path
-			else
-				@user = User.find(params[:id])
-			end
-		else
-			@user = User.find(session[:user_id])
-		end
+		redirect_to admin_panel_path if resource_user.admin?
 	end
 
 	def new
@@ -35,15 +26,31 @@ class UsersController < ApplicationController
 	end
 
 	def destroy
-		User.find(params[:required_user]).destroy
-		redirect_to root_path
+		resource_user.destroy
+		redirect_to admin_panel_path
 	end
 
 	def update
-
 	end
 
 	private
+
+	def resource_user
+		@resource_user ||=
+			if current_user.admin?
+					if params[:id].nil?
+						current_user
+					else
+						User.find(params[:id])
+					end
+				else
+					User.find(session[:user_id])
+			end
+	end
+
+	def semesters_collection
+		@semesters_collection ||= resource_user.semesters
+	end
 
 	def users_collection
 		@users_collection ||= User.all
