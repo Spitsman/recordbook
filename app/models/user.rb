cc02e2b4456342fc210115fc5b60c33543b58182
Григорий <kefan8@mail.ru>
2015-12-21 02:05:20 +0300

    поправил название кнопок как больше нравится, добавил еще кнопки в различных вьюхах, чтобы переходить сразу в админку(так удобнее, имхо), убрал валидацию пароля по длине, так как из-за нее почему-то не апдейтится юзер(надо выяснить, почему)

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,6 @@ class User < ActiveRecord::Base
 	has_secure_password
 	has_many :semesters
 	validates :email, :first_name, :last_name, presence: true
-	validates :password, { length: { minimum: 6 } }
 
 	def admin?
 		self.role == 'admin'
